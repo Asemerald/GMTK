5826655f4593182816b08616c8a964fb54450a7d
Ase <tdruesne05@gmail.com>
2025-07-31 12:49:07 +0200

    ça remarche

---
+++ Assets/Scripts/Runtime/Inputs/InputManager.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class InputManager : IDisposable
+{
+    private readonly InputActions input;
+
+    public event Action<ActionType> OnActionPressed;
+
+    public InputManager()
+    {
+        input = new InputActions();
+        input.Gameplay.Enable();
+
+        input.Gameplay.Direct.performed += _ => OnActionPressed?.Invoke(ActionType.Direct);
+        input.Gameplay.Crochet.performed += _ => OnActionPressed?.Invoke(ActionType.Crochet);
+        input.Gameplay.BlockLeft.performed += _ => OnActionPressed?.Invoke(ActionType.BlockLeft);
+        input.Gameplay.BlockRight.performed += _ => OnActionPressed?.Invoke(ActionType.BlockRight);
+    }
+
+    public void Dispose()
+    {
+        input.Dispose();
+    }
+}
