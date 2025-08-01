cf1957a368eb5f3a0acfecb1296b50ac1a42f268
pharreller <phafoubastien@gmail.com>
2025-08-01 16:47:36 +0200

    ActionHandler : probleme de queue trop courte

--- Assets/Scripts/Runtime/ScriptableObject/SO_ComboData.cs
+++ Assets/Scripts/Runtime/ScriptableObject/SO_ComboData.cs
@@ -8,8 +8,4 @@ public class SO_ComboData : ScriptableObject
     public SO_ActionData confirmationAction;
     public SO_ActionData openingAction;
     public List<SO_ActionData> comboActions;
-
-    [Tooltip("time : 0.25 = 1er quart temps, time : 2 = 2eme mesure ; value 0 = comboActions[0], value 3 = comboActions[3]")]
-    public AnimationCurve comboTimeline;  
 }
-
