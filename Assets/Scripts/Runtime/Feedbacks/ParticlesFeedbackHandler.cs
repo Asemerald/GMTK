e29347b66190241ea581ea8bf9177476fb8c58ea
Ase <tdruesne05@gmail.com>
2025-08-01 15:15:03 +0200

    Add FeedbackStructures

---
+++ Assets/Scripts/Runtime/Feedbacks/ParticlesFeedbackHandler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ParticleFeedbackHandler : MonoBehaviour, IFeedbackHandler
+{
+    [SerializeField] private ParticleSystem hitVFX;
+
+    public void PlayFeedback(string feedbackType)
+    {
+        if (feedbackType == "Hit") hitVFX.Play();
+    }
+}
---
+++ Assets/Scripts/Runtime/Feedbacks/PostProcessFeedbackHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PostProcessFeedbackHandler : MonoBehaviour, IFeedbackHandler
+{
+    [SerializeField] private Volume volume;
+    private ColorAdjustments colorAdjust;
+
+    private void Awake()
+    {
+        volume.profile.TryGet(out colorAdjust);
+    }
+
+    public void PlayFeedback(string feedbackType)
+    {
+        if (feedbackType == "Block") StartCoroutine(FlashHue());
+    }
+
+    private IEnumerator FlashHue()
+    {
+        colorAdjust.hueShift.value = 180;
+        yield return new WaitForSeconds(0.2f);
+        colorAdjust.hueShift.value = 0;
+    }
+}
